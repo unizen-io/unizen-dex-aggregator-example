0074e50709519f22ec5e28f7896db53953c73614
minapham17 <mina@unizen.io>
2024-12-11 18:26:36 +0700

    fix: updated contract address for Unizen router

--- utils/config/address.ts
+++ utils/config/address.ts
@@ -1,17 +1,19 @@
 
+import PROD_CONTRACT_ADDRESS from '@unizen-io/unizen-contract-addresses/production.json';
+
 import { SupportedChainID } from './token';
 
 const UNIZEN_ROUTER_ADDRESS: {
   [key in SupportedChainID]: string;
 } = {
-  [SupportedChainID.EthereumMainnet]: '0x0',
-  [SupportedChainID.BSCMainnet]: '0x8d463636D26cDde64f8142E5b741e1a20Fcd97Bc',
-  [SupportedChainID.PolygonMainnet]: '0x12265487B3b4034656Dd980B92886e892db3c17B',
-  [SupportedChainID.Fantom]: '0x0',
-  [SupportedChainID.Avalanche]: '0x0C1dd4253C4a157D015de34BBA5E489b6F420E34',
-  [SupportedChainID.Arbitrum]: '0xb2a18894e693DaA604460333543A2963311D9d40',
-  [SupportedChainID.Optimism]: '0xFC380cbF5f4ACE571313d55beF5Cf4F14F05Aa22',
-  [SupportedChainID.Base]: '0xed442cCfde7596b46Deb923620A2FEf9999976Af'
+  [SupportedChainID.EthereumMainnet]: PROD_CONTRACT_ADDRESS.unizenRouter.ethereum,
+  [SupportedChainID.BSCMainnet]: PROD_CONTRACT_ADDRESS.unizenRouter.bsc,
+  [SupportedChainID.PolygonMainnet]: PROD_CONTRACT_ADDRESS.unizenRouter.polygon,
+  [SupportedChainID.Fantom]: PROD_CONTRACT_ADDRESS.unizenRouter.fantom,
+  [SupportedChainID.Avalanche]: PROD_CONTRACT_ADDRESS.unizenRouter.avax,
+  [SupportedChainID.Arbitrum]: PROD_CONTRACT_ADDRESS.unizenRouter.arbitrum,
+  [SupportedChainID.Optimism]: PROD_CONTRACT_ADDRESS.unizenRouter.optimism,
+  [SupportedChainID.Base]: PROD_CONTRACT_ADDRESS.unizenRouter.base
 };
 
 export { UNIZEN_ROUTER_ADDRESS };
